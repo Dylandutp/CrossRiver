1a6d8f6d2c2984173fb7e51923361721069acc4f
Jesse1201 <101391317+Jesse1201@users.noreply.github.com>
2022-06-08 11:33:35 +0800

    Update AI_game.py

--- AI_game.py
+++ AI_game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 FPS = 60
 WHITE = (255, 255, 255)
@@ -6,6 +7,7 @@ GREEN = (102, 205, 0)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 BROWN = (139, 115, 85)
+LightSkyBlue = (135, 206, 250)
 WIDTH = 1000
 HEIGHT = 600
 # 遊戲初始化 and 創建視窗
@@ -14,95 +16,102 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("傳教士過河遊戲")
 clock = pygame.time.Clock()
 
+# 載入圖片
+boat_img = pygame.image.load(os.path.join("img", "boat.png")).convert()
+coast_left_img = pygame.image.load(os.path.join("img", "coast_left.png")).convert()
+coast_right_img = pygame.image.load(os.path.join("img", "coast_right.png")).convert()
+missionary_img = pygame.image.load(os.path.join("img", "missionaries.png")).convert()
+cannibal_img = pygame.image.load(os.path.join("img", "cannibals.png")).convert()
+
 class Coast1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((200, 100))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(coast_left_img, (500, 368))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = 0
-        self.rect.y = 500
+        self.rect.y = 232
 
 class Coast2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((200, 100))
-        self.image.fill(GREEN)
+        self.image = pygame.transform.scale(coast_right_img, (500, 368))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 800
-        self.rect.y = 500
+        self.rect.x = 500
+        self.rect.y = 232
 
 class Cannibal1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLACK)
+        self.image = pygame.transform.scale(cannibal_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 10
-        self.rect.y = 460        
+        self.rect.x = 920
+        self.rect.y = 435        
 
 class Cannibal2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLACK)
+        self.image = pygame.transform.scale(cannibal_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 40
-        self.rect.y = 460 
+        self.rect.x = 870
+        self.rect.y = 435
 
 class Cannibal3(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLACK)
+        self.image = pygame.transform.scale(cannibal_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 70
-        self.rect.y = 460
+        self.rect.x = 820
+        self.rect.y = 435
 
 class Missionary1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLUE)
+        self.image = pygame.transform.scale(missionary_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 460
+        self.rect.x = 770
+        self.rect.y = 435
 
 class Missionary2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLUE)
+        self.image = pygame.transform.scale(missionary_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 130
-        self.rect.y = 460
+        self.rect.x = 720
+        self.rect.y = 435
 
 class Missionary3(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 40))
-        self.image.fill(BLUE)
+        self.image = pygame.transform.scale(missionary_img, (80,80))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 160
-        self.rect.y = 460
+        self.rect.x = 670
+        self.rect.y = 435
 
 class Boat1(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(BROWN)
+        self.image = pygame.transform.scale(boat_img, (180,180))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 220
-        self.rect.y = 510
+        self.rect.x = 600
+        self.rect.y = 440
 
 class Boat2(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(BROWN)
+        self.image = pygame.transform.scale(boat_img, (180,180))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x = 290
-        self.rect.y = 510
+        self.rect.x = 540
+        self.rect.y = 350
 
 
 all_sprites = pygame.sprite.Group() 
@@ -140,7 +149,10 @@ while running:
     # all_sprites.update()
 
     # 畫面顯示
-    screen.fill(WHITE)
+    screen.fill(LightSkyBlue)
+    # screen.blit(coast_left_img, (10, 460))
+    # screen.blit(coast_right_img, (10, 460))
+    # screen.blit(cannibal_img, (10, 460))
     all_sprites.draw(screen)
     pygame.display.update()
-pygame.quit()            
+pygame.quit()             
